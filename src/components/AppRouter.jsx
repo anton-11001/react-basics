59055597085d831897c632e082c7deb0d8e85233
Anton <krava34007@gmail.com>
2022-09-09 07:43:09 +0000

    add routing

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { privateRoutes, publicRoutes } from "../router";
+import { AuthContext } from "../context";
+import Loader from "./UI/Loader/Loader";
+
+const AppRouter = () => {
+  const { isAuth, isLoading } = useContext(AuthContext);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isAuth) {
+    return (
+      <Switch>
+        {privateRoutes.map((route) => (
+          <Route
+            component={route.component}
+            path={route.path}
+            exact={route.exact}
+            key={route.path}
+          />
+        ))}
+        <Redirect to="/posts" />
+      </Switch>
+    );
+  }
+
+  return (
+    <Switch>
+      {publicRoutes.map((route) => (
+        <Route
+          component={route.component}
+          path={route.path}
+          exact={route.exact}
+          key={route.path}
+        />
+      ))}
+      <Redirect to="/login" />
+    </Switch>
+  );
+};
+
+export default AppRouter;
