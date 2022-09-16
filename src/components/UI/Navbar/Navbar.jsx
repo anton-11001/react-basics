9f9b84045af148a9d5da0ac6d086ecf66de86611
Anton <krava34007@gmail.com>
2022-09-16 07:43:09 +0000

    add navbar

---
+++ src/components/UI/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import MyButton from "../button/Button";
+import { AuthContext } from "../../../context";
+import classes from "./Navbar.module.css";
+
+const links = [
+  { to: "/about", label: "About" },
+  { to: "/posts", label: "Posts" },
+];
+
+const Navbar = ({ className = "" }) => {
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+
+  const logout = () => {
+    setIsAuth(false);
+    localStorage.removeItem("auth");
+  };
+
+  return (
+    <header className={[classes.navbar, className].join(" ")}>
+      <Link className={classes.brand} to={isAuth ? "/posts" : "/login"}>
+        React Basics
+      </Link>
+
+      <nav className={classes.links} aria-label="Main navigation">
+        {links.map((link) => (
+          <NavLink
+            key={link.to}
+            className={classes.link}
+            activeClassName={classes.active}
+            exact={link.exact ?? true}
+            to={link.to}
+          >
+            {link.label}
+          </NavLink>
+        ))}
+      </nav>
+
+      <div className={classes.actions}>
+        {isAuth ? (
+          <MyButton variant="outline" size="sm" onClick={logout}>
+            Logout
+          </MyButton>
+        ) : (
+          <Link className={classes.loginLink} to="/login">
+            Login
+          </Link>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
