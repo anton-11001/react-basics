04d43553aaf8c5b140f8fcbb7904ba5f8d616330
Anton <krava34007@gmail.com>
2022-10-27 09:27:41 +0000

    update privateRoutes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
-import About from "../pages/About";
-import PostsLazyLoading from "../pages/PostsLazyLoading";
-import PostsPagination from "../pages/PostsPagination";
+import PostsLazyLoading from "../pages/Posts/PostsLazyLoading";
+import PostsPagination from "../pages/Posts/PostsPagination";
 import PostIdPage from "../pages/PostIdPage/PostIdPage";
 import Login from "../pages/Login/Login";
 import Error from "../pages/Error/index";
 
 export const privateRoutes = [
-  { path: "/about", component: About, exact: true },
   { path: "/posts", component: PostsLazyLoading, exact: true },
   { path: "/posts-pagination", component: PostsPagination, exact: true },
   { path: "/posts/:id", component: PostIdPage, exact: true },
