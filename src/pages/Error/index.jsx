fb5734c8185a6ff23030dd4ab6e4bd0ac94c1cbd
Anton <krava34007@gmail.com>
2022-11-09 07:43:09 +0000

    add error page

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Error.module.css";
+
+const Error = () => {
+  return (
+    <div className={classes.page}>
+      <p className={classes.subtitle}>Page not found</p>
+    </div>
+  );
+};
+
+export default Error;
