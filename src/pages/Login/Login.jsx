fb0720a63cc9da71c5c78e0397b96656e3fb9dbe
Anton <krava34007@gmail.com>
2022-12-11 09:27:41 +0000

    fix login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import Input from "../../components/UI/input/Input";
 import Button from "../../components/UI/button/Button";
 import { AuthContext } from "../../context";
@@ -7,10 +8,38 @@ import classes from "./Login.module.css";
 const Login = () => {
   const { setIsAuth } = useContext(AuthContext);
 
+  const history = useHistory();
+
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
   const login = (event) => {
     event.preventDefault();
+
+    if (!credentials.username.trim() || !credentials.password.trim()) {
+      setError("Username and password are required.");
+      return;
+    }
+
+    setError("");
     setIsAuth(true);
     localStorage.setItem("auth", "true");
+
+    setCredentials({ username: "", password: "" });
+
+    history.push("/posts");
+  };
+
+  const updateCredentials = (field, value) => {
+    setCredentials({ ...credentials, [field]: value });
+
+    if (error) {
+      setError("");
+    }
   };
 
   return (
@@ -27,6 +56,10 @@ const Login = () => {
           <span className={classes.label}>Username</span>
           <Input
             type="text"
+            value={credentials.username}
+            onChange={(event) =>
+              updateCredentials("username", event.target.value)
+            }
             placeholder="Enter username"
             autoComplete="username"
           />
@@ -36,10 +69,16 @@ const Login = () => {
           <span className={classes.label}>Password</span>
           <Input
             type="password"
+            value={credentials.password}
+            onChange={(event) =>
+              updateCredentials("password", event.target.value)
+            }
             placeholder="Enter password"
             autoComplete="current-password"
           />
         </label>
+
+        {error && <p className={classes.error}>{error}</p>}
 
         <Button className={classes.submit} type="submit">
           Sign in
