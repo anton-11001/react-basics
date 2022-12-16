d3fdb04da59721d92e1d2b3ac6dec4389ad0bcff
Anton <krava34007@gmail.com>
2022-12-16 09:27:41 +0000

    fix post animations

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -12,7 +12,7 @@ const PostList = ({ posts, title, remove }) => {
       <h1 style={{ textAlign: "center" }}>{title}</h1>
       <TransitionGroup>
         {posts.map((post, index) => (
-          <CSSTransition key={post.id} timeout={500} classNames="post">
+          <CSSTransition key={post.id} timeout={240} classNames="post">
             <PostItem remove={remove} number={index + 1} post={post} />
           </CSSTransition>
         ))}
